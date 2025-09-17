41277f66ba06805eebe0e555d749e49b478ab5f9
Nora Moreau <nora.moreau66@gmail.com>
2025-09-17 14:11:33 +0200

    <feat> : <configuration nuxt et tailwind + structure de la page lycée.vue>

---
+++ frontend/app/pages/lycee.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4">
+    Lycée
+  </div>
+  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4">
+    Classe
+  </div>
+  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4">
+    Spécialité
+  </div>
+  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4">
+    Notes
+  </div>
+</template>
+
+<script setup>
+
+</script>
