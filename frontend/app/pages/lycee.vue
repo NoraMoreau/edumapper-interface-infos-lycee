14309256286f7d6ca0cebbbb33490aa6fef1b521
Nora Moreau <nora.moreau66@gmail.com>
2025-09-17 14:56:34 +0200

    <feat> : <Ajout des boutons de la page lycee.vue>

--- frontend/app/pages/lycee.vue
+++ frontend/app/pages/lycee.vue
@@ -1,18 +1,48 @@
 <template>
   <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4">
     Lycée
+    <div class="mt-4">
+        <button @click="modifierLycee()" class="px-4 py-2 bg-white rounded-full">Modifier</button>
+    </div>
   </div>
-  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4">
+
+  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4 flex justify-between items-center">
     Classe
+    <div>
+        <button @click="aCompleter()" class="text-sm">À compléter</button>
+    </div>
   </div>
-  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4">
+
+  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4 flex justify-between items-center">
     Spécialité
+    <div>
+        <button @click="aCompleter()" class="text-sm">À compléter</button>
+    </div>
   </div>
-  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4">
+
+  <div class=" bg-blue-100 p-3 border rounded mx-4 mt-4 flex justify-between items-center">
     Notes
+    <div>
+        <button @click="aCompleter()" class="text-sm">À compléter</button>
+    </div>
   </div>
+  
+  <div class="fixed bottom-12 left-1/2 transform -translate-x-1/2">
+    <button @click="aConfirmer()" class="px-4 py-2 bg-gray-200 rounded-full">Confirmer</button>
+  </div>  
 </template>
 
 <script setup>
 
+function modifierLycee() {
+
+}
+
+function aCompleter() {
+    
+}
+
+function aConfirmer() {
+
+}
 </script>
